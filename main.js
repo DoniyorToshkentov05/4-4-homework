04495a38aa048d403a6f39ba1db3609343f8eb7b
Doniyortoshkentov05 <doniyortoshkentov@gmail.com>
2023-08-26 23:48:24 +0500

    end

---
+++ main.js
@@ -0,0 +1,317 @@
+// let = 'lenovo';
+// switch (laptop) {
+//     case 'asus':
+//         console.log('700$');
+//         break;
+//     case 'mac':
+//         console.log('1500$');
+//         break;
+//     case 'mac':
+//         console.log('1000$');
+//         break;
+//     case 'hp':
+//         console.log('950$');
+//         break;
+//    
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+
+
+
+
+
+
+// class Phone {
+
+//     constructor(name, price, display, camera, ram){
+//         this.name = name;
+//         this.price = price;
+//         this.display = display;
+//         this.camera = camera;
+//         this.ram = ram;
+//     }
+
+//     phone(){
+//         console.log(this.name + ' ' + this.price);
+//     }
+
+// }
+// let iphone14 = new Phone('Iphone 13 pro', '10 053 000 sum', '2000 nit', '48 MP')
+
+// let samsungs22 = new Phone('Samsung S21', '$950', '1300 nit', '10 MP', '8 GB')
+
+// let asusrogphone = new Phone('Redmi 12 pro', '$340', '1000 nit', '50 MP', '8 GB')
+
+// let pocox5 = new Phone('Poco X5 PRO', '$350', '1500 nit', '108 MP', '8 GB')
+
+
+
+
+
+
+
+// let watch = new Date();
+// let havta = watch.getDate();
+// let kun = watch .getDate();
+// let soat = watch .getHours();
+// let min = watch .getMinutes();
+
+
+
+
+// switch (a,b) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+
+
+// let = 5;
+
+// switch (5-1) {
+//    case 1:
+//       console.log(5-1);
+//       break;
+// }
+
+// 13
+
+// let = 5;
+
+// switch (5+3-2) {
+//    case 1:
+//       console.log(5+3-2);
+//       break;
+// }
+
+
+
+
+
+// let a = 6;
+// let b = 9;
+
+// switch (a > b) {
+//    case value:
+//       console.log(a < b);
+//       break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let a = 15;
+// let b = 15;
+// switch (true) {
+//     case a % 2 == 0 && b % 2 == 0:
+//         console.log('ikkisi ham juft');
+//         break;
+//     case a % 2 != 0 && b % 2 == 0:
+//         console.log('a soni toq');
+//         break;
+//     case a % 2 == 0 && b % 2 != 0:
+//         console.log('b soni toq');
+//         break;
+//     case a % 2 != 0 && b % 2 != 0:
+//         console.log('ikkisi ham toq');
+//         break;
+//     default:
+//         console.log('bu son emas');
+//         break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let a = 7;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(a);
+//       break;
+//    case 2:   
+//       console.log(b);
+//       break;
+//    case 3:
+//       console.log(c);
+//       break;
+// }
+
+
+
+
+
+
+// let a = 9;
+// let b = 6;
+// let c = 5;
+
+// switch (a,b,c) {
+//    case 1:
+//       console.log(b);
+//       break;
+//    case 2:   
+//       console.log(c);
+//       break;
+//    case 3:
+//       console.log(a);
+//       break;
+// }
+
+
+
+
+// class Gentra extends Car{
+//    constructor(name,model,weight,speed,abs,airback){
+//       super(name,model,weight,speed)
+//       this.abs = abs;
+//       this.airback = airback;
+//    }
+// }
+
+// let Gentra = new Daewoo("Gentra",4,'1.5 tonna',220,true,true);
+
+
+// let Gentra = new Car("Gentra",3,'1.8 tonna',200);
+
+
+
+
+
+
+
+
+// let PC = new PC("Pc",3,' 2kg ',0);
+
+// class book extends{
+//    constructor(name,model,weight,speed,abs,airback){
+//       super(name,model,weight,speed)
+//       this.abs = abs;
+//       this.airback = airback;
+//    }
+// }
+
+
+
+// class more extends phone{
+//     constructor(name,model,ram,chip,storage,camera,battery){
+//         super(name,model,ram,chip,storage);
+//         this.camera=camera;
+//         this.battery=battery;
+//     }
+//     getCamera(){
+//         console.log(
+//             this.camera + ' ' + this.battery
+//         );
+//     }
+// }
+// let iphone = new more('Iphone','XS',3,'A12','64GB','12MP','2658 mAh');
+
+
+
+// let a = 13;
+// let b = 13;
+// let c = 13;
+
+// switch (true) {
+//     case a > b && a > c:
+//         console.log('a soni katta');
+//         break;
+//     case b > a && b > c:
+//         console.log('b soni katta');
+//         break;
+//     case c > b && c > a:
+//         console.log('c soni katta');
+//         break;
+//     default:
+//         console.log('notogri raqam kirittingz yoki barchasi teng');
+//         break;
+// }
+
+
+
+
+
+
+
+// let num = 5;
+
+// switch (true) {
+//     case num > 0:
+//         console.log(num+3);
+//         break;
+//     case num < 0:
+//         console.log(2+num);
+//         break;
+//     default:
+//         console.log('iltimos son kiriting');
+//         break;
+// }
+
+
+
+
+
+
+
+// let a = 9;
+// let b =9;
+// switch (true) {
+//     case a > b:
+//         console.log('a katta');
+//         break;
+//     case b > a:
+//         console.log('b katta');
+//         break;
+//     case a == b:
+//         console.log('a va b teng');
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
+
+
+
+// let a = 9;
+// let b =9;
+// switch (true) {
+//     case a > b:
+//         console.log('a katta');
+//         break;
+//     case b > a:
+//         console.log('b katta');
+//         break;
+//     case a == b:
+//         console.log('a va b teng');
+//         break;
+//     default:
+//         console.log(false);
+//         break;
+// }
